b74101aec99100f3781ea097afbda9b9025b241c
user <user@LAPTOP-LGH1B1F4>
2021-06-07 20:24:19 +0700

    buat tampilan "Buat Proposal"

--- src/modalmudah/FXMLDocumentController.java
+++ src/modalmudah/FXMLDocumentController.java
@@ -23,9 +23,7 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("ndemek aku!");
-        System.out.println("ndemek aku2");
-        System.out.println("ndemek aku3");
+        
         label.setText("Hello World!");
     }
     

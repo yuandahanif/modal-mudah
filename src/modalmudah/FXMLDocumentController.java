723e16f7e127d84084d359cf391ac375c0705266
user <user@LAPTOP-LGH1B1F4>
2021-06-07 19:44:41 +0700

    uwes tak ganti

--- src/modalmudah/FXMLDocumentController.java
+++ src/modalmudah/FXMLDocumentController.java
@@ -23,7 +23,7 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
+        System.out.println("ndemek aku!");
         label.setText("Hello World!");
     }
     

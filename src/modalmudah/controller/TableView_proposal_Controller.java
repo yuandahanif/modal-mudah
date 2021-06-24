c6bc7fcf7312b12f62205ae6589cbdb3fe4bcf03
user <user@LAPTOP-LGH1B1F4>
2021-06-24 16:15:43 +0700

    nyoba buat css dan multipleScene dan multiStage:v

--- src/modalmudah/controller/TableView_proposal_Controller.java
+++ src/modalmudah/controller/TableView_proposal_Controller.java
@@ -21,6 +21,17 @@ import javafx.scene.input.MouseEvent;
 import modalmudah.helper.xstream;
 import modalmudah.model.Proposal;
 
+ import javafx.scene.Parent;
+ import javafx.scene.Scene;
+ import javafx.stage.Stage;
+ import javafx.stage.StageStyle;
+ import javafx.stage.Modality;
+ import java.io.IOException;
+ import java.util.ResourceBundle;
+ import javafx.fxml.FXMLLoader;
+ import javafx.stage.Stage;
+ import javafx.scene.Scene;
+
 /**
  * FXML Controller class
  *
@@ -63,6 +74,22 @@ public class TableView_proposal_Controller implements Initializable {
         ubah.setDisable(false);
     }
     
+//    @FXML
+//    private void handleLoginAction(MouseEvent event) throws IOException {
+//        FXMLLoader loader = new FXMLLoader();
+//        loader.setLocation(Window2_update_Controller.class.getResource("update_proposal_view.fxml")); 
+//        //Praktikum1 diganti dengan nama file controller untuk FXML yang kedua
+//        // FXML2 diganti dengan FXML yang kedua
+//
+//        Stage dialogStage = new Stage(); 
+//        dialogStage.setTitle("testing");
+//        dialogStage.initModality(Modality.APPLICATION_MODAL); 
+//        Scene scene = new Scene(loader.load()); 
+//        dialogStage.setScene(scene);
+//        // Show the dialog and wait until the user closes it 
+//        dialogStage.showAndWait();
+//    }
+    
     //tombol hapus
     @FXML
     private void handleHapusButtonAction(ActionEvent event) {
@@ -70,7 +97,7 @@ public class TableView_proposal_Controller implements Initializable {
         allProposals = proposal_T.getItems();
         P = proposal_T.getSelectionModel().getSelectedItems();
         
-        // hapus daryi tabel
+        // hapus dari tabel
         P.forEach(allProposals::remove);
         proposal_T.getSelectionModel().select(null);
         

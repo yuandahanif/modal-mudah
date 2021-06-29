e3f47ab58fdf7b017e215b1710c845136f3968db
yuan <34027873+yuandahanif@users.noreply.github.com>
2021-06-30 00:12:30 +0700

    bisa buka popup update

--- src/modalmudah/controller/TableView_proposal_Controller.java
+++ src/modalmudah/controller/TableView_proposal_Controller.java
@@ -21,16 +21,15 @@ import javafx.scene.input.MouseEvent;
 import modalmudah.helper.xstream;
 import modalmudah.model.Proposal;
 
- import javafx.scene.Parent;
- import javafx.scene.Scene;
- import javafx.stage.Stage;
- import javafx.stage.StageStyle;
- import javafx.stage.Modality;
- import java.io.IOException;
- import java.util.ResourceBundle;
- import javafx.fxml.FXMLLoader;
- import javafx.stage.Stage;
- import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.Modality;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
 
 /**
  * FXML Controller class
@@ -67,45 +66,45 @@ public class TableView_proposal_Controller implements Initializable {
         } catch (NumberFormatException e) {
         }
     }
-    
+
     @FXML
     private void handleTableClick(MouseEvent event) {
         hapus.setDisable(false);
         ubah.setDisable(false);
     }
-    
-//    @FXML
-//    private void handleLoginAction(MouseEvent event) throws IOException {
-//        FXMLLoader loader = new FXMLLoader();
-//        loader.setLocation(Window2_update_Controller.class.getResource("update_proposal_view.fxml")); 
-//        //Praktikum1 diganti dengan nama file controller untuk FXML yang kedua
-//        // FXML2 diganti dengan FXML yang kedua
-//
-//        Stage dialogStage = new Stage(); 
-//        dialogStage.setTitle("testing");
-//        dialogStage.initModality(Modality.APPLICATION_MODAL); 
-//        Scene scene = new Scene(loader.load()); 
-//        dialogStage.setScene(scene);
-//        // Show the dialog and wait until the user closes it 
-//        dialogStage.showAndWait();
-//    }
-    
+
+    @FXML
+    private void handleLoginAction(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(modalmudah.ModalMudah.class.getResource("/modalmudah/view/" + "update_proposal_view.fxml"));
+        //Praktikum1 diganti dengan nama file controller untuk FXML yang kedua
+        // FXML2 diganti dengan FXML yang kedua
+
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle("testing");
+        dialogStage.initModality(Modality.APPLICATION_MODAL);
+        Scene scene = new Scene(loader.load());
+        dialogStage.setScene(scene);
+        // Show the dialog and wait until the user closes it
+        dialogStage.showAndWait();
+    }
+
     //tombol hapus
     @FXML
     private void handleHapusButtonAction(ActionEvent event) {
         ObservableList<Proposal> P, allProposals;
         allProposals = proposal_T.getItems();
         P = proposal_T.getSelectionModel().getSelectedItems();
-        
+
         // hapus dari tabel
         P.forEach(allProposals::remove);
         proposal_T.getSelectionModel().select(null);
-        
+
         // save to xml
         ArrayList<Proposal> dataProposalbaru = new ArrayList<>(allProposals);
         dataXml.saveToXML(dataProposalbaru);
     }
-    
+
     /**
      * Initializes the controller class.
      *

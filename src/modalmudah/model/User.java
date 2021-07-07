7ac6ddcbd02fc0fe47ead4f2e5fb27d29532dfd1
yuan <34027873+yuandahanif@users.noreply.github.com>
2021-07-07 18:48:16 +0700

    buat model user

--- src/modalmudah/model/User.java
+++ src/modalmudah/model/User.java
@@ -10,5 +10,50 @@ package modalmudah.model;
  * @author yuan
  */
 public class User {
-    
+
+    private final Integer id;
+    private final String no_id;
+    private String email;
+    private String name;
+    private String password;
+
+    public User(Integer id, String no_id, String email, String name, String password) {
+        this.id = id;
+        this.no_id = no_id;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNo_id() {
+        return no_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
